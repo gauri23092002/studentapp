07a376cb01f789a5c78d022aae6d95b2b5e09695
gauri23092002 <gauri23092002@gmail.com>
2022-07-08 10:11:54 +0530

    studentapp created

---
+++ src/app/viewstudent/viewstudent.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewstudent',
+  templateUrl: './viewstudent.component.html',
+  styleUrls: ['./viewstudent.component.css']
+})
+export class ViewstudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+name="anu"
+
+}
